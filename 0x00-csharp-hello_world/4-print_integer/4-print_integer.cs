54317e31b9ea9dd1a0042e047a0914181bf0a451
andergcp <andergcp@hotmail.com>
2020-11-30 20:42:04 -0500

    Just printing number

--- 0x00-csharp-hello_world/4-print_integer/4-print_integer.cs
+++ 0x00-csharp-hello_world/4-print_integer/4-print_integer.cs
@@ -2,9 +2,9 @@
 
 class Program
 {
-    static void Main(string[] args)
-    {
-        int number = 972;
-        Console.WriteLine($"{number,0:D} Mission Street");
-    }
+	static void Main(string[] args)
+	{
+		int number = 972;
+		Console.WriteLine($"{number} Mission Street");
+	}
 }
