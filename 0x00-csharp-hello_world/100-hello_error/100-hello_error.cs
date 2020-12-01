4a888b59213e6d7a90f3ea2330482674c7491f92
andergcp <andergcp@hotmail.com>
2020-11-30 21:50:30 -0500

    Task 12

---
+++ 0x00-csharp-hello_world/100-hello_error/100-hello_error.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace _100_hello_error
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Environment.ExitCode = 1;
+            Console.Error.WriteLine("Hello World!");
+        }
+    }
+}
