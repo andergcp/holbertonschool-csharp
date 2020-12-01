68efee2525ad60f7f7a562e348fd6286520214f7
andergcp <andergcp@hotmail.com>
2020-11-30 21:07:59 -0500

    Task 6

---
+++ 0x00-csharp-hello_world/6-print_string/6-print_string.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        string str = "Holberton School";
+        string splited = str.Substring(0, 9);
+        Console.WriteLine("{0}{0}{0}", str);
+        Console.WriteLine($"{splited}");
+    }
+}
