c4a4499ceeb80231f8dec0ce445573ea7ff9fc54
andergcp <andergcp@hotmail.com>
2020-12-04 18:32:01 -0500

    Task 0

---
+++ 0x02-csharp-arrays_lists_dictionaries/0-print_array/0-print_array.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+class Array
+{
+    public static int[] CreatePrint(int size)
+    {
+        if (size < 0)
+        {
+            Console.WriteLine("Size cannot be negative");
+            return null;
+        }
+        else
+        {
+            var newArray = new int[size];
+            string sep = "";
+            for (var i = 0; i < newArray.Length; i++)
+            {
+                newArray[i] = i;
+                Console.Write($"{sep}{i}");
+                sep = " ";
+            }
+            Console.WriteLine();
+            return newArray;
+        }
+    }
+}
