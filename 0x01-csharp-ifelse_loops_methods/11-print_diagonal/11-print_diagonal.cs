78868efeeae2c8e3274b510ac55c1ba4e567ce6e
andergcp <andergcp@hotmail.com>
2020-12-04 15:55:31 -0500

    Task 11

---
+++ 0x01-csharp-ifelse_loops_methods/11-print_diagonal/11-print_diagonal.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+class Line
+{
+    public static void PrintDiagonal(int length)
+    {
+        if (length > 0)
+            for (var i = 0; i < length; i++)
+            {
+                Console.Write(new string(' ', i));
+                Console.Write("\\\n");
+            }
+        Console.Write("\n");
+    }
+}
