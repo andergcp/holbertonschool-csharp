1826afb3ce00d25d210fb7b0b1385dd5f02502dc
andergcp <andergcp@hotmail.com>
2020-12-04 18:42:24 -0500

    Task 2

---
+++ 0x02-csharp-arrays_lists_dictionaries/2-replace_element/2-replace_element.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+class Array
+{
+    public static int[] ReplaceElement(int[] array, int index, int n)
+    {
+        var len = array.Length;
+        if (index < 0 || index >= len)
+        {
+            Console.WriteLine("Index out of range");
+            return array;
+        }
+        else
+        {
+            array[index] = n;
+            return array;
+        }
+    }
+}
