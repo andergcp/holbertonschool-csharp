fe99a6ca6ab91301559cc40014f445bc219ff27c
andergcp <andergcp@hotmail.com>
2020-12-04 18:37:28 -0500

    Task 1

---
+++ 0x02-csharp-arrays_lists_dictionaries/1-element_at/1-element_at.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+class Array
+{
+    public static int elementAt(int[] array, int index)
+    {
+        int len = array.Length;
+        if (index < 0 || index >= len)
+        {
+            Console.WriteLine("Index out of range");
+            return -1;
+        }
+        else
+        {
+            return array[index];
+        }
+    }
+}
