936a519346929bd38fd5d85c0db0c50b2bbaeca3
andergcp <andergcp@hotmail.com>
2020-12-04 18:54:42 -0500

    Task 3

---
+++ 0x02-csharp-arrays_lists_dictionaries/3-print_array_reverse/3-print_array_reverse.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+class Array
+{
+    public static void Reverse(int[] array)
+    {
+        if (array == null || array.Length == 0)
+        {
+            Console.WriteLine();
+        }
+        else
+        {
+            int len = array.Length;
+            string sep = "";
+            for (var i = len - 1; i >= 0; i--)
+            {
+                Console.Write($"{sep}{array[i]}");
+                sep = " ";
+            }
+            Console.WriteLine();
+        }
+    }
+}
