a605f02a191b798d1636ea19b9b195f4f2f025c3
andergcp <andergcp@hotmail.com>
2020-12-04 15:14:35 -0500

    Task 6

---
+++ 0x01-csharp-ifelse_loops_methods/6-print_comb2/6-print_comb2.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace _6_print_comb2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var sep = "";
+            for (var i = 0; i <= 9; i++)
+            {
+                for (var j = 0; j <= 9; j++)
+                {
+                    if (j > i)
+                    {
+                        Console.Write($"{sep}{i}{j}");
+                        sep = ", ";
+                    }
+                }
+            }
+            Console.Write("\n");
+        }
+    }
+}
