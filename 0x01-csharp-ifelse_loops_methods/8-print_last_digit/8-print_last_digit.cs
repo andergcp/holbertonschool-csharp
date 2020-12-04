2cceaf39809f5ed2fbdd25d96b533692685949bc
andergcp <andergcp@hotmail.com>
2020-12-04 15:27:36 -0500

    Task 8

---
+++ 0x01-csharp-ifelse_loops_methods/8-print_last_digit/8-print_last_digit.cs
@@ -0,0 +1,12 @@
+﻿using System;
+class Number
+{
+    public static int PrintLastDigit(int number)
+    {
+        int lastDigit = number % 10;
+        if (lastDigit < 0)
+            lastDigit *= -1;
+        Console.Write(lastDigit);
+        return lastDigit;
+    }
+}
