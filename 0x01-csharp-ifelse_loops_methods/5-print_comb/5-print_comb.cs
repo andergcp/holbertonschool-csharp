d535fcb22d89de04d16695191916f96f456abd53
andergcp <andergcp@hotmail.com>
2020-12-04 15:04:32 -0500

    Task 5

---
+++ 0x01-csharp-ifelse_loops_methods/5-print_comb/5-print_comb.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace _4_print_hexa
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var sep = "";
+            for (var i = 0; i < 100; i++)
+            {
+                Console.Write($"{sep}{i:D2}");
+                sep = ", ";
+            }
+        }
+    }
+}
