7a248cc6401370f2526b4af869ba1e72211d87b5
andergcp <andergcp@hotmail.com>
2020-12-04 20:43:44 -0500

    Task 8

---
+++ 0x02-csharp-arrays_lists_dictionaries/8-number_keys/8-number_keys.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+
+class Dictionary
+{
+    public static int NumberOfKeys(Dictionary<string, string> myDict)
+    {
+        int counter = 0;
+        foreach (var key in myDict)
+            counter += 1;
+        return counter;
+    }
+}
