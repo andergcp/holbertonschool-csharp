17781d5e6c76711a1e861e4d2a609cf8cb3b5a32
andergcp <andergcp@hotmail.com>
2020-12-04 19:06:02 -0500

    Task 4

---
+++ 0x02-csharp-arrays_lists_dictionaries/4-print_list/4-print_list.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+
+
+class List
+{
+    public static List<int> CreatePrint(int size)
+    {
+        if (size < 0)
+        {
+            Console.WriteLine("Size cannot be negative");
+            return null;
+        }
+        else
+        {
+            var newList = new List<int>();
+            string sep = "";
+            for (var i = 0; i < size; i++)
+            {
+                newList.Add(i);
+                Console.Write($"{sep}{i}");
+                sep = " ";
+            }
+            Console.WriteLine();
+            return newList;
+        }
+    }
+}
