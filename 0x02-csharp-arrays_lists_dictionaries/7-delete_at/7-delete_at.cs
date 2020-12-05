50a0725903bb2baf8c3d792569453038d7a961ce
andergcp <andergcp@hotmail.com>
2020-12-04 20:26:35 -0500

    Task 7

---
+++ 0x02-csharp-arrays_lists_dictionaries/7-delete_at/7-delete_at.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+
+
+class List
+{
+    public static List<int> DeleteAt(List<int> myList, int index)
+    {
+        int len = myList.Count;
+        if (index < 0 || index >= len)
+            Console.WriteLine("Index is out of range");
+        else
+            for (int i = index; i < len; i++)
+            {
+                if (i + 1 < len)
+                    myList[i] = myList[i + 1];
+                else
+                    myList.RemoveRange(i - 1, 1);
+            }
+        return myList;
+    }
+}
