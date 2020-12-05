b5e95aa3f481c7875213d90e1bf25fc927b12bcb
andergcp <andergcp@hotmail.com>
2020-12-04 20:32:25 -0500

    Simplifying code

--- 0x02-csharp-arrays_lists_dictionaries/7-delete_at/7-delete_at.cs
+++ 0x02-csharp-arrays_lists_dictionaries/7-delete_at/7-delete_at.cs
@@ -10,13 +10,7 @@ class List
         if (index < 0 || index >= len)
             Console.WriteLine("Index is out of range");
         else
-            for (int i = index; i < len; i++)
-            {
-                if (i + 1 < len)
-                    myList[i] = myList[i + 1];
-                else
-                    myList.RemoveRange(i - 1, 1);
-            }
+            myList.Remove(myList[index]);
         return myList;
     }
 }
