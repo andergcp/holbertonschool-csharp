3e593103b4dedfb46879e55a0305476048b5d7b1
andergcp <andergcp@hotmail.com>
2020-12-04 20:01:14 -0500

    Task 6

---
+++ 0x02-csharp-arrays_lists_dictionaries/6-divisible_by_2/6-divisible_by_2.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+
+class List
+{
+    public static List<bool> DivisibleBy2(List<int> myList)
+    {
+        var resultList = new List<bool>();
+        foreach (int number in myList)
+        {
+            bool ans = number % 2 == 0 ? true : false;
+            resultList.Add(ans);
+        }
+        return resultList;
+    }
+}
