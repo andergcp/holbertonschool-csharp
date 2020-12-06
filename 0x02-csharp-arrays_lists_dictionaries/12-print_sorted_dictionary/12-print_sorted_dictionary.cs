19fb8ee91802c20e809a1369450e9415634053fe
andergcp <andergcp@hotmail.com>
2020-12-06 18:37:18 -0500

    Task 12

---
+++ 0x02-csharp-arrays_lists_dictionaries/12-print_sorted_dictionary/12-print_sorted_dictionary.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+class Dictionary
+{
+    public static void PrintSorted(Dictionary<string, string> myDict)
+    {
+        var listKeys = myDict.Keys.ToList();
+        listKeys.Sort();
+
+        foreach (var key in listKeys)
+        {
+            Console.WriteLine("{0}: {1}", key, myDict[key]);
+        }
+    }
+}
