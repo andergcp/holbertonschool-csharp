cd6e52df5bc90de30dd47f3eefb8df16238368ef
andergcp <andergcp@hotmail.com>
2020-12-06 20:15:21 -0500

    Task 14

---
+++ 14-rectangular_array/14-rectangular_array.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace _14_rectangular_array
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[,] newArray = new int[5, 5];
+            array[2, 2] = 1;
+            for (int i = 0; i < 5; i++)
+            {
+                for (int j = 0; j < 5; j++)
+                {
+                    if (j < 4)
+                        Console.Write(array[i, j] + " ");
+                    else
+                        Console.Write(array[i, j]);
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+}
