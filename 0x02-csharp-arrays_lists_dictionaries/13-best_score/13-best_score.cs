d831f2e41573540a7d3a6575ff187abada9a99ec
andergcp <andergcp@hotmail.com>
2020-12-06 20:01:25 -0500

    Task 13

---
+++ 0x02-csharp-arrays_lists_dictionaries/13-best_score/13-best_score.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+
+class Dictionary
+{
+    public static string BestScore(Dictionary<string, int> myList)
+    {
+        string maxKey = "None";
+        int maxValue = 0;
+        foreach (var item in myList)
+        {
+            if (item.Value > maxValue)
+            {
+                maxKey = item.Key;
+                maxValue = item.Value;
+            }
+        }
+        return maxKey;
+    }
+}
